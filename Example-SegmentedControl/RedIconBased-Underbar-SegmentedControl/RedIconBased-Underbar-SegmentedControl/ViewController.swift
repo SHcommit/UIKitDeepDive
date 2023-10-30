cc8df25e6c7e0f0d3907716651fefe115107ae9c
shcommit <happysh_s2@naver.com>
2023-10-30 23:07:16 +0900

    [Add] 프로젝트 생성

---
+++ Example-SegmentedControl/RedIconBased-Underbar-SegmentedControl/RedIconBased-Underbar-SegmentedControl/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  RedIconBased-Underbar-SegmentedControl
+//
+//  Created by 양승현 on 10/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .white
+  }
+}
